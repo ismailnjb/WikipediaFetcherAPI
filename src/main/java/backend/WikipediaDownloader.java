32ec105ba7342225a8105a4295b1c0643106d075
ismailnjb <mohammed.ismail.njb@gmail.com>
2024-10-19 15:02:14 +0530

    task manager update

--- src/main/java/backend/WikipediaDownloader.java
+++ src/main/java/backend/WikipediaDownloader.java
@@ -17,6 +17,8 @@ public class WikipediaDownloader implements Runnable {
 
     public static void main(String[] args) {
         TaskManager taskManager=new TaskManager(20);
+        WikipediaDownloader wikipediaDownloader=new WikipediaDownloader(("Albert_Einstein"));
+        taskManager.waitTillQueueIsFreeAndAddTask(wikipediaDownloader);
 
     }
 }
